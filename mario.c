6c94783229e43e904e96f94bd9db88706a87eba1
AndrewSmithWasTaken <AndrewSmithWasTaken@users.noreply.github.com>
2015-08-11 13:35:29 +0300

    Update mario.c
    Tidied and commented

--- mario.c
+++ mario.c
@@ -3,11 +3,11 @@
 
 int main(void)
 {
-
     int height;
     int space;
     int hash;
     
+    //getting the height of the wall and error checking
     do
     {
         printf("please enter a non-negative integer no greater than 23: ");
@@ -15,6 +15,7 @@ int main(void)
     }
     while ((height < 0) || (height > 23));
     
+    //iterating through the height values, printing the relevant numbers of spaces and octothorpes
     for (int i=1; i<=height; i++)
     {
         for (space = (height - i); space > 0; space--)
@@ -25,8 +26,6 @@ int main(void)
         {   
             printf("#"); 
         }
- 
-        printf("\n");
+         printf("\n");
     }
-
 }
